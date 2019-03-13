a1a1692cfd9f4243b28ed1d333950a567882167a
Jake Wharton <jakew@google.com>
2019-03-13 17:43:00 -0400

    Add some JavaPoet Kotlin helpers
    Update BaseLayoutBinderWriter to use them.
    
    Bug: 128314294
    Test: existing databinding
    
    Change-Id: I3cd249b7a7d34c7078d31cb1515bc8cd578a2d8c

--- compilerCommon/src/main/kotlin/android/databinding/tool/ext/javapoet_ext.kt
+++ compilerCommon/src/main/kotlin/android/databinding/tool/ext/javapoet_ext.kt
@@ -13,7 +13,47 @@
 
 package android.databinding.tool.ext
 
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.ParameterSpec
+import com.squareup.javapoet.TypeName
+import com.squareup.javapoet.TypeSpec
+
 const val L = "\$L"
 const val T = "\$T"
 const val N = "\$N"
 const val S = "\$S"
+
+fun javaFile(
+    packageName: String,
+    typeSpec: TypeSpec,
+    body: JavaFile.Builder.() -> Unit
+): JavaFile = JavaFile.builder(packageName, typeSpec).apply(body).build()
+
+fun classSpec(
+    name: ClassName,
+    body: TypeSpec.Builder.() -> Unit = {}
+): TypeSpec = TypeSpec.classBuilder(name).apply(body).build()
+
+fun constructorSpec(
+    body: MethodSpec.Builder.() -> Unit = {}
+): MethodSpec = MethodSpec.constructorBuilder().apply(body).build()
+
+fun methodSpec(
+    name: String,
+    body: MethodSpec.Builder.() -> Unit = {}
+): MethodSpec = MethodSpec.methodBuilder(name).apply(body).build()
+
+fun parameterSpec(
+    type: TypeName,
+    name: String,
+    body: ParameterSpec.Builder.() -> Unit = {}
+): ParameterSpec = ParameterSpec.builder(type, name).apply(body).build()
+
+fun fieldSpec(
+    name: String,
+    type: TypeName,
+    body: FieldSpec.Builder.() -> Unit = {}
+): FieldSpec = FieldSpec.builder(type, name).apply(body).build()
